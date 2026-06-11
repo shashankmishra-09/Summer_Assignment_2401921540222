e1b5472f2df79d3ce2b7081f12219e8f092abb57
Shashank Mishra <shashankmishra8335@gmail.com>
2026-06-12 00:53:29 +0530

    code-1.cpp

--- Week_2/Day_3/code-1.cpp
+++ Week_2/Day_3/code-1.cpp
@@ -2,9 +2,7 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int index = -1;
         for(int i=0;i<haystack.size();i++){
-            index = i;
             string ans = "";
             for(int j=i;j<haystack.size();j++){
                 char ch = haystack[j];
