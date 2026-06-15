427a29a53be60e75de00eab516981f215805f63f
Shashank Mishra <shashankmishra8335@gmail.com>
2026-06-15 23:27:35 +0530

    code-2.cpp

---
+++ Week_3/Day_1/code-2.cpp
@@ -0,0 +1,16 @@
+// REVERSE LINKED LIST 
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* temp = head;
+        ListNode* prev = nullptr;
+        while(temp!=nullptr){
+            ListNode* front = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
+    }
+};
