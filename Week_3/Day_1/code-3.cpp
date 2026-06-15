0f350d0c01866b2e7383999a7627fb5deb5742f5
Shashank Mishra <shashankmishra8335@gmail.com>
2026-06-15 23:28:56 +0530

    code-3.cpp

---
+++ Week_3/Day_1/code-3.cpp
@@ -0,0 +1,14 @@
+// MIDDLE OF THEE LINKED LIST
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
